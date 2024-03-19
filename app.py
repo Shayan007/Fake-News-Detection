eceb0da05539affca35c644f94a4382e715a4d0c
Md Shayan Shafique <shafiqueshayan@gmail.com>
2024-03-19 14:02:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import re
-import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
